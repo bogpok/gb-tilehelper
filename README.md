ea4f7ca28f92f7fea931fdc50d2d450e5466f453
Bogdan Pokrepin <bogpok@ya.ru>
2023-06-29 10:37:56 +0300

    add documentation

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # Game Boy Tile helper JS
 JS code to swipe tile ids when you use fonts
 
+## How to use
+1. Copy your ```.c``` mapset file into the folder containing ```swipe.js```. The mapset file should contain only one ```unsigned char bg2[]```.
+2. Rename ```.c``` to be ```tilemap_input.c```.
+3. Run swipe.js:
+```bash
+node swipe.js
+```
+4. Now you got ```tilemap_output.c```. Check if all values inside charset is OK.
+5. Rename ```tilemap_output.c``` as you wish and move it to your destination folder.
+
+
 ## Explanation
-When font is loaded in GB it occupies first 36 places (0x24)
+For example, when font is loaded in GB memory it occupies first 36 places (0x24).
 
 What the JS code does:
 - reads .c file to find ONE map character array
-- remake all tile ids in tilemap array
+- shift all tile ids in tilemap array to the right for **36** places
+
+You can modify .js code to shift for less/more places.
+
--- swipe.js
+++ swipe.js
@@ -2,8 +2,16 @@
   USE NODE JS
 */
 
+
+console.log('Start swiping...\n');
+
+/**
+ * Shifts all entries in charArray for swipeN places
+ * @param  {Array} charArray All entries should be strings with Dex numbers
+ * @param  {number} swipeN Integer number of places to shift
+ * @return {Array}      Shifted array
+ */
 function swipeRight(charArray, swipeN = 36){
-  
   let number = new String;
   for (let i=0; i<charArray.length; i++) {
     let spaces = '';
@@ -13,17 +21,14 @@ function swipeRight(charArray, swipeN = 36){
       number = ns.slice(ns.length-4,ns.length)
     } else { 
       number = ns 
-    }
-    
+    }    
     charArray[i] = spaces + '0x' + (parseInt(number,16) + swipeN).toString(16);
-
   }
-
   return charArray
 }
 
+// INPUT
 const fs = require('fs');
-
 const data = fs.readFileSync('./tilemap_input.c', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
@@ -31,7 +36,8 @@ const data = fs.readFileSync('./tilemap_input.c', 'utf8', (err, data) => {
   }
 });
 
-//console.log(typeof(data)); // string
+//#region Get array with map_array entries
+// Get string
 let start = 0;
 let end = 0;
 let write = false;
@@ -47,28 +53,37 @@ for (let i=0; i < data.length; i++){
     write = false;
   }  
 }
-
+// Get array
 let charArray = newstring.split(",");
-console.log("Initial lenght: ", charArray.length);
-let swipedArr = swipeRight(charArray, swipeN=-1);
-console.log("Result lenght: ", swipedArr.length);
-console.log(swipedArr);
+//#endregion
 
+// Swipe all values to the right
+// You can specify swipeN here!
+// e.g. swipeRight(charArray, swipeN=-1);
+let swipedArr = swipeRight(charArray);
 
-// The map array
-let content1 = swipedArr.toString();
-fs.writeFile('./arrdata.txt', content1, err => {
+// Check sums
+if (charArray.length!==swipedArr.length) {
+  console.warn("Length of input and output arrays are not equal!");
+  console.warn("Initial lenght: ", charArray.length);
+  console.warn("Result lenght: ", swipedArr.length);
+}
+
+let writeData = (content, outputFileName) => {
+  fs.writeFile(outputFileName, content, err => {
   if (err) {
     console.error(err);
   }
   // file written successfully
-});
+  });
+}
 
+// The map_array
+let content1 = swipedArr.toString();
 // Everything else
 let content2 = data.slice(0, start+1) + content1 + data.slice(end, data.length)
-fs.writeFile('./tilemap_output.c', content2, err => {
-  if (err) {
-    console.error(err);
-  }
-  // file written successfully
-});
+
+writeData(content1, './arrdata.txt');
+writeData(content2, './tilemap_output.c');
+
+console.log("Fin!\n");
